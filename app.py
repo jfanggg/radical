5d9def902f3b00106d4a43efde6032dc1109465d
John <fang97j@gmail.com>
2016-12-18 01:06:39 -0600

    Refactored a lot. Basic searching for characters using parameters works

--- app.py
+++ app.py
@@ -1,42 +1,86 @@
 from models import Character, Database
-from flask import Flask, send_file
+from flask import Flask, send_file, abort
 from flask_restful import Resource, Api
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs
 
-# constants
+# Constants
 db_path = "resources/wiki_processed.txt"
 
-# initialize data
+# Helper functions
+def int_tuple(iterable):
+  return tuple(int(element) for element in iterable)
+
+def chr_list(iterable):
+  return [chr(element) for element in iterable]
+
+def ord_tuple(iterable):
+  return tuple(ord(element) for element in iterable)
+
 def initialize_db():
   db = Database()
   with open(db_path, mode="r", encoding="utf-16") as db_file:
     for line in db_file:
-      string, kind, part1, part2 = line.split(", ")
-      character = Character(ord(string), kind, [part1, part2])
+      parts = line.strip().split(", ")
+
+      cp    = ord(parts[0])
+      kind  = int(parts[1])
+      part1 = ord_tuple(parts[2])
+      part2 = ord_tuple(parts[3])
+
+      character = Character(cp, kind, part1, part2)
       db.add_character(character)
+
   return db
 
-db = initialize_db()
-  
-# initialize Flask
+# Flask things
 app = Flask(__name__)
 api = Api(app)
 
-# Flask-RESTful
 class CharacterResource(Resource):
+  """ resource for looking up a specific, known character. """
   def get(self, char_cp):
     character = db.find_character(char_cp)
+
+    if character is None:
+      abort(404)
+
     return {
-      "string": chr(character.cp),
+      "character": chr(character.cp),
       "kind": character.kind,
-      "part1": character.parts[0],
-      "part2": character.parts[1]
+      "part1": chr_list(character.part1),
+      "part2": chr_list(character.part2)
+    }
+
+class CharactersResource(Resource):
+  """ resource for looking up a set of characters via parameters. """
+
+  characters_args = {
+    "kind": fields.Int(validate=validate.Range(min=1, max=11)),
+    "part1": fields.String(),
+    "part2": fields.String()
+  }
+
+  #TODO: add a limiter. Like only return 50 characters or something
+
+  @use_kwargs(characters_args)
+  def get(self, kind, part1, part2):
+    part1 = int_tuple(part1.split("-")) if part1 else None
+    part2 = int_tuple(part2.split("-")) if part2 else None
+
+    characters = db.find_characters(kind, part1, part2)
+
+    return {
+      "characters": chr_list([character.cp for character in characters])
     }
 
 @app.route("/")
 def index():
-    return send_file("templates/index.html")
+  return send_file("templates/index.html")
 
 api.add_resource(CharacterResource, "/char/<int:char_cp>")
+api.add_resource(CharactersResource, "/chars/")
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+  db = initialize_db()
+  app.run("0.0.0.0")
--- models.py
+++ models.py
@@ -1,34 +1,50 @@
 class Character:
-  def __init__(self, cp, kind, parts):
-    self.cp     = cp      # int   (code point)
-    self.kind   = kind    # int   (kind number)
-    self.parts  = parts   # [int] (components' code points)
+  def __init__(self, cp, kind, part1, part2):
+    self.cp     = cp      # int       (code point)
+    self.kind   = kind    # int       (kind number)
+    self.part1  = part1   # (int)     (components' code points)
+    self.part2  = part2   # (int)     (components' code points)
 
 class Database:
   def __init__(self):
-    self.characters = {}  # int -> Character   (code point)
-    self.part_map   = {}  # int -> [Character] (code points)
-    self.kind_map   = {}  # int -> [Character] (kind number)
+    self.characters = {}  # int     -> Character   (code point)
+    self.part1_map  = {}  # (int)   -> [Character] (code points)
+    self.part2_map  = {}  # (int)   -> [Character] (code points)
+    self.kind_map   = {}  # int     -> [Character] (kind number)
+
+  @staticmethod
+  def update_map(mapping, key, value):
+    """ Updates a map where the value is a list to either create or append to.
+    Used specifically for part1, part2 and kind maps"""
+    if key in mapping:
+      mapping[key].append(value)
+    else:
+      mapping[key] = [value]
+    return mapping
 
   def add_character(self, char):
     """ Given a Character object, add it to the database's maps """
-
-    # characters
     self.characters[char.cp] = char
 
-    # part map
-    for part in char.parts:
-      if part in self.part_map:
-        self.part_map[part].append(char)
-      else:
-        self.part_map[part] = [char]
-
-    # kind map
-    if char.kind in self.kind_map:
-      self.kind_map[char.kind].append(char)
-    else:
-      self.kind_map[char.kind] = [char]
+    self.part1_map  = Database.update_map(self.part1_map, char.part1, char)
+    self.part2_map  = Database.update_map(self.part2_map, char.part2, char)
+    self.kind_map   = Database.update_map(self.kind_map, char.kind, char)
   
-  # a simple find method for testing. Will be replaced later
   def find_character(self, cp):
-    return self.characters[cp]
+    """ Finds a specific character using a code point """
+    return self.characters.get(cp, None)
+    
+  def find_characters(self, kind=None, part1=None, part2=None):
+    """ Finds a set of characters given certain criteria """
+    matches = set(self.characters.values())
+
+    if kind:
+      matches &= set(self.kind_map[kind])
+
+    if part1:
+      matches &= set(self.part1_map[part1])
+
+    if part2:
+      matches &= set(self.part2_map[part2])
+  
+    return matches
