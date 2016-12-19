11d543a1d780ee3278fdd34c3cc24d93a13c9c78
John <fang97j@gmail.com>
2016-12-19 00:22:11 -0600

    Added a basic compose page, similar to the decompose one

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-"use strict";   // See note about "use strict"; below
+"use strict";
 
 var app = angular.module("app", [
  "ngRoute",
@@ -31,8 +31,33 @@ app.config(["$routeProvider",
 app.controller("indexController", function($scope) {
 
 });
-app.controller("composeController", function($scope) {
+app.controller("composeController", function($scope, $http) {
+  // input
+  $scope.kind = 0;
+  $scope.part1 = "";
+  $scope.part2 = "";
 
+  // output
+  $scope.characters = [];
+
+  $scope.composeCharacters = function() {
+    console.log("scope kind is " + $scope.kind)
+    var url = "/api/chars/?"
+    if ($scope.kind !== 0) {
+      url += "kind=" + $scope.kind.toString() + "&";
+    }
+    if ($scope.part1 !== "") {
+      url += "part1=" + $scope.part1.charCodeAt(0).toString() + "&";
+    }
+    if ($scope.part2 !== "") {
+      url += "part2=" + $scope.part2.charCodeAt(0).toString();
+    }
+
+    $http.get(url)
+    .then(function (response) { 
+      $scope.characters = response.data.characters;
+    });
+  }
 });
 app.controller("decomposeController", function($scope, $http) {
   // input
