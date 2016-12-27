0cdbeae7dcf9262d9f0cf7b159f0be6fe84fc5fb
John Fang <fang15j@ncssm.edu>
2016-12-27 17:21:04 -0600

    Started on Decompose. Pushing to work on Ubuntu side

--- static/js/decomposeController.js
+++ static/js/decomposeController.js
@@ -1,19 +1,33 @@
 angular.module("app").controller("decomposeController", function($scope, $http) {
-  // input
-  $scope.character = "";
+    // input
+    $scope.character = "";
 
-  // output
-  $scope.kind = 0;
-  $scope.part1 = "";
-  $scope.part2 = "";
+    // output
+    $scope.kind = -1;
+    $scope.part1 = "";
+    $scope.part2 = "";
 
-  $scope.decomposeCharacter = function() {
-    var cp = $scope.character.charCodeAt(0);
-    $http.get("/api/char/" + cp.toString())
-    .then(function (response) { 
-      $scope.kind = response.data.kind;
-      $scope.part1 = response.data.part1;
-      $scope.part2 = response.data.part2;
-    });
-  };
+  
+    $scope.hasPart2 = function() {
+        if ($scope.kind === -1) {
+            return true;
+        }
+        else {
+            var array = [
+                false, true, true, true, true, true, false, false, true, true, false
+            ];
+            return array[$scope.kind];
+        }
+    }
+    
+    $scope.decomposeCharacter = function() {
+        var cp = $scope.character.charCodeAt(0);
+        $http.get("/api/char/" + cp.toString())
+            .then(function (response) { 
+                $scope.kind = response.data.kind;
+                $scope.part1 = response.data.part1;
+                $scope.part2 = response.data.part2;
+            }
+        );
+    };
 });
