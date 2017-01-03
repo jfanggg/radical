86eacdbbd8aed5f5552dcc9bd841d9ba4584b43a
John <fang97j@gmail.com>
2017-01-03 17:59:56 -0600

    Optimized Python models to use sets instead

--- models.py
+++ models.py
@@ -7,24 +7,26 @@ class Character:
 
 class Database:
   def __init__(self):
-    self.characters = {}  # int     -> Character   (code point)
-    self.part1_map  = {}  # (int)   -> [Character] (code points)
-    self.part2_map  = {}  # (int)   -> [Character] (code points)
-    self.kind_map   = {}  # int     -> [Character] (kind number)
+    self.char_set   = set() # {Character}
+    self.char_map   = {}    # int     -> Character
+    self.part1_map  = {}    # (int)   -> {Character}
+    self.part2_map  = {}    # (int)   -> {Character}
+    self.kind_map   = {}    # int     -> {Character}
 
   @staticmethod
   def update_map(mapping, key, value):
-    """ Updates a map where the value is a list to either create or append to.
+    """ Updates a map where the value is a set to either create or add to.
     Used specifically for part1, part2 and kind maps"""
     if key in mapping:
-      mapping[key].append(value)
+      mapping[key].add(value)
     else:
-      mapping[key] = [value]
+      mapping[key] = {value}
     return mapping
 
   def add_character(self, char):
     """ Given a Character object, add it to the database's maps """
-    self.characters[char.cp] = char
+    self.char_set.add(char);
+    self.char_map[char.cp] = char
 
     self.part1_map  = Database.update_map(self.part1_map, char.part1, char)
     self.part2_map  = Database.update_map(self.part2_map, char.part2, char)
@@ -32,19 +34,19 @@ class Database:
   
   def find_character(self, cp):
     """ Finds a specific character using a code point """
-    return self.characters.get(cp, None)
+    return self.char_map.get(cp, None)
     
   def find_characters(self, kind=-1, part1=None, part2=None):
     """ Finds a set of characters given certain criteria """
-    matches = set(self.characters.values())
+    matches = self.char_set
 
     if kind != -1:
-      matches &= set(self.kind_map.get(kind, []))
+      matches = matches & self.kind_map.get(kind, set())
 
     if part1:
-      matches &= set(self.part1_map.get(part1, []))
+      matches = matches & self.part1_map.get(part1, set())
 
     if part2:
-      matches &= set(self.part2_map.get(part2, []))
+      matches = matches & self.part2_map.get(part2, set())
   
     return matches
